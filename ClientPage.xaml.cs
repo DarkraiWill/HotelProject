ed376c34696eaaf9e88ffdd9d16437535eb6b617
Will <152715654+DarkraiWill@users.noreply.github.com>
2025-02-09 15:05:19 +0300

    client

---
+++ ClientPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace UP2.Pages
+{
+    public partial class ClientPage : Page
+    {
+        HotelEntities HE = HotelEntities.GetContext();
+        List<Guest> ClientsList;
+        public ClientPage()
+        {
+            InitializeComponent();
+            ClientsList = HE.Guests.ToList();
+            
+            DGridClients.ItemsSource = ClientsList;
+        }
+
+    }
+}
